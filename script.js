bf1161cef2e9268da52def3dda5f91abedb0c390
Jahongir7 <Jahongir95119@gmail.com>
2021-07-31 21:55:58 +0500

    yangi repo uchun

---
+++ script.js
@@ -0,0 +1,85 @@
+const hourEl = document.querySelector(".hour");
+const minutEl = document.querySelector(".minut");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const toggle = document.querySelector(".toggle");
+const dateEl = document.querySelector(".date");
+
+const days = [
+  "Dushanba",
+  "Seshanba",
+  "Chorshanba",
+  "Payshanba",
+  "Juma",
+  "Shanba",
+  "Yakshanba",
+];
+const months = [
+  "Yanvar",
+  "Fevral",
+  "Mart",
+  "May",
+  "Iyun",
+  "Iyul",
+  "Avgust",
+  "Sentabr",
+  "Oktabr",
+  "Noyabr",
+  "Dekabr",
+];
+
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Tungi Rejim";
+    console.log("clicked");
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Kunduzgi Rejim";
+  }
+});
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const hours = time.getHours();
+  const date = time.getDate();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+  minutEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+setTime();
+
+setInterval(setTime, 1000);
